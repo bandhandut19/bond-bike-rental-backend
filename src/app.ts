279a83c622ddda724ba2719457042094fe672129
bandhandut19 <bondon21081@gmail.com>
2024-06-16 21:43:41 +0600

    initial setup done

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import bodyParser from 'body-parser'
 const app: Application = express()
 
 //parsers
 app.use(express.json())
 app.use(cors())
+app.use(bodyParser.json())
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
