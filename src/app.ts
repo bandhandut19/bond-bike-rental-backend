3455a781432e7a27d664da9349af6ab7f8a3ae2e
bandhandut19 <bondon21081@gmail.com>
2024-09-30 17:02:37 +0600

    changed to production mode

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,8 @@ import cookieParser from 'cookie-parser';
 const app: Application = express();
 
 const corsOptions = {
-  origin: 'http://localhost:5173', // Allow only this origin
-  // origin: 'https://bond-bike-rental-frontend.vercel.app', // Allow only this origin
+  // origin: 'http://localhost:5173', // Allow only this origin
+  origin: 'https://bond-bike-rental-frontend.vercel.app', // Allow only this origin
   credentials: true, // Allow credentials (cookies, authorization headers)
 };
 
@@ -20,11 +20,11 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  // res.header(
-  //   'Access-Control-Allow-Origin',
-  //   'https://bond-bike-rental-frontend.vercel.app',
-  // );
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://bond-bike-rental-frontend.vercel.app',
+  );
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
   res.header(
